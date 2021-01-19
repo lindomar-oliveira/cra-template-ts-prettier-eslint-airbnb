49bbdf5befd4d140f15014600a2d5c0a327b7c0c
Lindomar Oliveira <lindomar.souza1999@gmail.com>
2021-01-19 10:36:07 -0400

    Refactoring to eslint rules

--- template/src/reportWebVitals.ts
+++ template/src/reportWebVitals.ts
@@ -1,8 +1,14 @@
 import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFCP, getFID, getLCP, getTTFB }) => {
+    import('web-vitals').then(({
+      getCLS,
+      getFCP,
+      getFID,
+      getLCP,
+      getTTFB
+    }) => {
       getCLS(onPerfEntry);
       getFCP(onPerfEntry);
       getFID(onPerfEntry);
