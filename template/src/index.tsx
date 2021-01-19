6befc6480e93c923f24223c231b0724ea36446dd
Lindomar Oliveira <lindomar.souza1999@gmail.com>
2021-01-19 09:59:36 -0400

    Updated model based on cra standard

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -2,11 +2,16 @@ import { render } from 'react-dom';
 
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
-render(<App />, document.getElementById('root'));
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
